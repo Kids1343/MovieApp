eb1e1537bd6c2cbea2513a8645e8d99e378bc784
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2023-04-26 08:58:56 +0300

    add fly animation

---
+++ src/routes/search/[id].svelte
@@ -0,0 +1,22 @@
+<script context="module">
+    export async function load({fetch, params}){
+        const res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=<<api_key>>&language=en-US`);
+        const movieDetail = await res.json();
+        if(res.ok){
+            return{
+                props: {searchedMovie: movieDetail}
+            }
+        }
+    }   
+</script>
+
+<script>
+    import MovieCard from "../../components/MovieCard.svelte";
+    export let searchedMovie;
+</script>
+
+<div class="searched-movies">
+    {#each searchedMovie as movie}
+        <MovieCard {movie} />
+    {/each}
+</div>
